707ab36cc6c991a41e7e0cb79ef47c20df880934
Muhammad Ahsan Kaleem <muhammad.ahsan.kaleem@gmail.com>
2019-08-22 16:10:17 +0500

    Add files via upload

---
+++ Hydrargyrum_Switch.ino
@@ -0,0 +1,19 @@
+int buzzer = 3;
+int mercury = A0;
+int value;
+void setup() {
+  Serial.begin(9600);
+  pinMode(mercury, INPUT);
+}
+
+void loop() {
+  value = analogRead(mercury);
+  Serial.println(value);
+  if (value < 100) {
+      tone(buzzer, 5000);
+  }
+  else {
+    noTone(buzzer);
+  }
+  delay(100);
+}
